eed69f03f70d3f16b3aeb83d731bbe5ebe9d9ca3
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2022-10-27 20:55:44 +0200

    Menú principal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,96 @@
+import Controlers.AnadirObjetos;
+import Models.Alquiler;
+import Models.Libro;
+import Models.Usuario;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+import static Controlers.AnadirObjetos.anadirLibros;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Añadir objetos
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        ArrayList<Libro> libros = new ArrayList<>();
+        ArrayList<Alquiler> alquileres = new ArrayList<>();
+        libros = anadirLibros();
+        usuarios = AnadirObjetos.anadirUsuarios();
+        alquileres = AnadirObjetos.anadirAlquileres();
+
+        //Menú
+        int seleccion = 0;
+        while (seleccion == 7){
+            seleccion = menu();
+            switch (seleccion){
+                case 1:
+                    System.out.println("Alquilar libro");
+                    alquilarLibro();
+                    break;
+                case 2:
+                    System.out.println("Devolver libro");
+                    devolverLibro();
+                    break;
+                case 3:
+                    System.out.println("Generar estadísticas");
+                    generarEstadisticas();
+                    break;
+                case 4:
+                    System.out.println("Añadir Libros");
+                    anadirLibros();
+                    break;
+                case 5:
+                    System.out.println("Dar de alta usuarios");
+                    darAltaUsuario();
+                    break;
+                case 6:
+                    System.out.println("Exportar alquileres");
+                    exportarAlquileres();
+                    break;
+                case 7:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+            System.out.println("******************");
+        }
     }
+
+    public static int menu(){
+        int opcion;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Alquilar libro");
+        System.out.println("2. Devolver libro");
+        System.out.println("3. Generar estadísticas");
+        System.out.println("4. Añadir Libros");
+        System.out.println("5. Dar de alta usuarios");
+        System.out.println("6. Exportar alquileres");
+        System.out.println("7. Salir");
+        opcion = sc.nextInt();
+        return opcion;
+    }
+
+
+    private static void exportarAlquileres() {
+    }
+
+    private static void darAltaUsuario() {
+    }
+
+    private static void generarEstadisticas() {
+    }
+
+    private static void devolverLibro() {
+    }
+
+    private static void alquilarLibro() {
+    }
+
+
+
+
+
 }
