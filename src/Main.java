62fa89541052cead644eba43f0f56f6f82979871
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2022-10-29 11:51:47 +0200

    Corrección de errores en el menú

--- src/Main.java
+++ src/Main.java
@@ -15,44 +15,45 @@ public class Main {
         ArrayList<Usuario> usuarios = new ArrayList<>();
         ArrayList<Libro> libros = new ArrayList<>();
         ArrayList<Alquiler> alquileres = new ArrayList<>();
-        libros = anadirLibros();
+        libros = AnadirObjetos.anadirLibros();
         usuarios = AnadirObjetos.anadirUsuarios();
         alquileres = AnadirObjetos.anadirAlquileres();
 
         //Menú
         int seleccion = 0;
-        while (seleccion == 7){
+        while (seleccion != 7){
+            System.out.println("Menú de la biblioteca:");
             seleccion = menu();
             switch (seleccion){
                 case 1:
-                    System.out.println("Alquilar libro");
+                    System.out.println("⭐️ Alquilar libro -");
                     alquilarLibro();
                     break;
                 case 2:
-                    System.out.println("Devolver libro");
+                    System.out.println("️⭐️ Devolver libro ⭐️");
                     devolverLibro();
                     break;
                 case 3:
-                    System.out.println("Generar estadísticas");
+                    System.out.println("⭐️ Generar estadísticas ⭐️");
                     generarEstadisticas();
                     break;
                 case 4:
-                    System.out.println("Añadir Libros");
-                    anadirLibros();
+                    System.out.println("⭐️ Añadir Libro ⭐️");
+                    anadirLibro();
                     break;
                 case 5:
-                    System.out.println("Dar de alta usuarios");
+                    System.out.println("⭐️ Dar de alta usuarios ⭐️");
                     darAltaUsuario();
                     break;
                 case 6:
-                    System.out.println("Exportar alquileres");
+                    System.out.println("⭐️ Exportar alquileres ⭐️");
                     exportarAlquileres();
                     break;
                 case 7:
                     System.out.println("Saliendo...");
                     break;
                 default:
-                    System.out.println("Opcion no valida");
+                    System.out.println("Opcion no valida ⚠️");
                     break;
             }
             System.out.println("******************");
@@ -65,10 +66,11 @@ public class Main {
         System.out.println("1. Alquilar libro");
         System.out.println("2. Devolver libro");
         System.out.println("3. Generar estadísticas");
-        System.out.println("4. Añadir Libros");
+        System.out.println("4. Añadir Libro");
         System.out.println("5. Dar de alta usuarios");
         System.out.println("6. Exportar alquileres");
         System.out.println("7. Salir");
+        System.out.println("Escribe la opción deseada: ");
         opcion = sc.nextInt();
         return opcion;
     }
@@ -88,6 +90,11 @@ public class Main {
 
     private static void alquilarLibro() {
     }
+
+    private static void anadirLibro() {
+        System.out.println("Añadiendo libros");
+    }
+
 
 
 
