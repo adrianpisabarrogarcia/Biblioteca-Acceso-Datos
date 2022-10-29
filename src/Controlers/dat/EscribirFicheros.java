b52ee21254591f3e15e8d1cd81e9d01acfe3342c
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2022-10-29 11:46:50 +0200

    ✅ Write files

---
+++ src/Controlers/dat/EscribirFicheros.java
@@ -0,0 +1,96 @@
+package Controlers.dat;
+
+import Models.Alquiler;
+import Models.Libro;
+import Models.Usuario;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class EscribirFicheros {
+
+    /**
+     * Escribe un fichero binario con el contenido de un ArrayList de String
+     * @param arrayList - ArrayList de objetos de libros a escribir en el fichero
+     */
+    public static void escribirFicheroLibro(ArrayList<Libro> arrayList) {
+        String nombreFichero = "libros.dat";
+        try {
+            //Crear flujo de salida
+            FileOutputStream fileout = new FileOutputStream(nombreFichero);
+            //Crear flujo de datos
+            ObjectOutputStream dataOS = new ObjectOutputStream(fileout);
+
+            //Escribir datos en el fichero
+            for (Libro libro : arrayList) {
+                dataOS.writeObject(libro);
+            }
+
+            //Cerrar flujos
+            dataOS.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error al escribir el fichero");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Escribe un fichero binario con el contenido de un ArrayList de String
+     * @param arrayList - ArrayList de objetos de alquileres a escribir en el fichero
+     */
+    public static void escribirFicheroAlquiler(ArrayList<Alquiler> arrayList) {
+        String nombreFichero = "alquileres.dat";
+        try {
+            //Crear flujo de salida
+            FileOutputStream fileout = new FileOutputStream(nombreFichero);
+            //Crear flujo de datos
+            ObjectOutputStream dataOS = new ObjectOutputStream(fileout);
+
+            //Escribir datos en el fichero
+            for (Alquiler alquiler : arrayList) {
+                dataOS.writeObject(alquiler);
+            }
+
+            //Cerrar flujos
+            dataOS.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error al escribir el fichero");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Escribe un fichero binario con el contenido de un ArrayList de String
+     * @param arrayList - ArrayList de objetos de alquileres a escribir en el fichero
+     */
+    public static void escribirFicheroUsuario(ArrayList<Usuario> arrayList) {
+        String nombreFichero = "usuarios.dat";
+        try {
+            //Crear flujo de salida
+            FileOutputStream fileout = new FileOutputStream(nombreFichero);
+            //Crear flujo de datos
+            ObjectOutputStream dataOS = new ObjectOutputStream(fileout);
+
+            //Escribir datos en el fichero
+            for (Usuario usuario : arrayList) {
+                dataOS.writeObject(usuario);
+            }
+
+            //Cerrar flujos
+            dataOS.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error al escribir el fichero");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
